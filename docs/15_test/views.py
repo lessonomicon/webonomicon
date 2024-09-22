b4648f60cb17d172a5214b20ffe46cf6445588c5
Greg Wilson <gvwilson@third-bit.com>
2024-09-22 17:03:48 -0400

    updates after comments

--- docs/15_test/views.py
+++ docs/15_test/views.py
@@ -19,7 +19,7 @@ def experiments(data, staff_id):
     return _use_template("experiments.html", data, staff_id=staff_id)
 
 def heartbeat(data):
-    return render_template_string("<p>{{ message }}</p>", message=data['message'])
+    return render_template_string("<p>{{ message }}</p>", message=data["message"])
 
 
 def _use_template(template_name, data, **kwargs):
